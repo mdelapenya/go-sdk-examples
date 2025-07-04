bb687cce13677ce546c1711feb723348bf150413
Manuel de la Peña <mdelapenya@gmail.com>
2025-07-04 16:40:10 +0200

    feat: add examples for building images

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/mdelapenya/go-sdk-examples
 go 1.24.1
 
 require (
-	github.com/docker/go-sdk/client v0.1.0-alpha006
-	github.com/docker/go-sdk/container v0.1.0-alpha006
-	github.com/docker/go-sdk/context v0.1.0-alpha006
-	github.com/docker/go-sdk/image v0.1.0-alpha006
-	github.com/docker/go-sdk/network v0.1.0-alpha006
+	github.com/docker/go-sdk/client v0.1.0-alpha007
+	github.com/docker/go-sdk/container v0.1.0-alpha007
+	github.com/docker/go-sdk/context v0.1.0-alpha007
+	github.com/docker/go-sdk/image v0.1.0-alpha007
+	github.com/docker/go-sdk/network v0.1.0-alpha007
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/caarlos0/env/v11 v11.3.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
@@ -22,14 +23,22 @@ require (
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/docker v28.2.2+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
-	github.com/docker/go-sdk/config v0.1.0-alpha006 // indirect
+	github.com/docker/go-sdk/config v0.1.0-alpha007 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/go-archive v0.1.0 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.6.0 // indirect
+	github.com/moby/sys/user v0.4.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- images/images_test.go
+++ images/images_test.go
@@ -1,14 +1,116 @@
 package images
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"strings"
 
+	"github.com/docker/go-sdk/client"
 	"github.com/docker/go-sdk/image"
 )
 
-func Example_readImagesFromDockerfile() {
+func Example_build_fromContext() {
+	buf := bytes.NewBuffer(nil)
+	cli, err := client.New(context.Background(), client.WithLogger(slog.New(slog.NewTextHandler(buf, nil))))
+	if err != nil {
+		log.Println("Error creating client:", err)
+		return
+	}
+
+	reader, err := image.ArchiveBuildContext("testdata/multiple", "Dockerfile")
+	if err != nil {
+		log.Println("Error creating build context:", err)
+		return
+	}
+
+	tag, err := image.Build(
+		context.Background(),
+		reader,
+		"my-image:context",
+		image.WithBuildClient(cli),
+	)
+	if err != nil {
+		log.Println("Error building image:", err)
+		return
+	}
+
+	fmt.Println(tag)
+
+	// Output:
+	// my-image:context
+}
+
+func Example_buildFromDir_simple() {
+	buf := bytes.NewBuffer(nil)
+	cli, err := client.New(context.Background(), client.WithLogger(slog.New(slog.NewTextHandler(buf, nil))))
+	if err != nil {
+		log.Println("Error creating client:", err)
+		return
+	}
+
+	tag, err := image.BuildFromDir(
+		context.Background(),
+		"testdata/simple",
+		"Dockerfile",
+		"my-image:simple",
+		image.WithBuildClient(cli),
+	)
+	if err != nil {
+		log.Println("Error building image:", err)
+		return
+	}
+
+	fmt.Println(tag)
+
+	// Output:
+	// my-image:simple
+}
+
+func Example_buildFromDir_multiple() {
+	buf := bytes.NewBuffer(nil)
+	cli, err := client.New(context.Background(), client.WithLogger(slog.New(slog.NewTextHandler(buf, nil))))
+	if err != nil {
+		log.Println("Error creating client:", err)
+		return
+	}
+
+	tag, err := image.BuildFromDir(
+		context.Background(),
+		"testdata/multiple",
+		"Dockerfile",
+		"my-image:multiple",
+		image.WithBuildClient(cli),
+	)
+	if err != nil {
+		log.Println("Error building image:", err)
+		return
+	}
+
+	fmt.Println(tag)
+
+	// Output:
+	// my-image:multiple
+}
+
+func Example_imagesFromDockerfile() {
+	images, err := image.ImagesFromDockerfile("testdata/complex/Dockerfile", nil)
+	if err != nil {
+		log.Println("Error reading images from Dockerfile:", err)
+		return
+	}
+
+	for _, img := range images {
+		fmt.Println(img)
+	}
+
+	// Output:
+	// golang:1.23-alpine@sha256:f8113c4b13e2a8b3a168dceaee88ac27743cc84e959f43b9dbd2291e9c3f57a0
+}
+
+func Example_imagesFromReader() {
 	dockerfile := `FROM alpine:latest
 FROM busybox:latest
 FROM ${MY_IMAGE}`
