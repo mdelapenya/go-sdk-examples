13868689e4844f09216744a6953483a70fe9e371
Manuel de la Peña <mdelapenya@gmail.com>
2025-07-09 14:33:02 +0200

    chore(context): add examples

---
+++ context/context_test.go
@@ -0,0 +1,68 @@
+package context
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/docker/go-sdk/context"
+)
+
+func ExampleCurrent() {
+	ctx, err := context.Current()
+	fmt.Println(err)
+	fmt.Println(ctx != "")
+
+	// Output:
+	// <nil>
+	// true
+}
+
+func ExampleCurrentDockerHost() {
+	host, err := context.CurrentDockerHost()
+	fmt.Println(err)
+	fmt.Println(host != "")
+
+	// Output:
+	// <nil>
+	// true
+}
+
+func ExampleDockerHostFromContext() {
+	host, err := context.DockerHostFromContext("desktop-linux")
+	if err != nil {
+		log.Printf("error getting docker host from context: %s", err)
+		return
+	}
+
+	fmt.Println(host)
+
+	// Intentionally not printing the output, as the context could not exist in the CI environment
+}
+
+func ExampleList() {
+	contexts, err := context.List()
+	if err != nil {
+		log.Printf("error listing contexts: %s", err)
+		return
+	}
+
+	fmt.Println(len(contexts) > 0)
+
+	// Output:
+	// true
+}
+
+func ExampleInspect() {
+	ctx, err := context.Inspect("docker-cloud")
+	if err != nil {
+		log.Printf("error inspecting context: %s", err)
+		return
+	}
+
+	fmt.Println(ctx.Name)
+	fmt.Println(ctx.Context.Description)
+	fmt.Println(ctx.Context.Field("otel"))
+	fmt.Println(ctx.Context.Fields())
+
+	// Intentionally not printing the output, as the context could not exist in the CI environment
+}
